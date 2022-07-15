1c94fbbba5ec85d54a644da8a0ec07a9631b6950
Najmul890 <najmul890890@gmail.com>
2022-07-15 07:39:15 -0700

    create a book and delete a book functionality completed

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addBook } from '../Books/BookSlice';
 
 const AddBook = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const dispatch= useDispatch();
+    const navigate= useNavigate();
+
+    const numberOfBooks= useSelector((state)=> state.booksReducer.books.length);
+
+    const onSubmit = data => {
+        const id=numberOfBooks+1;
+        data.id=id;
+        dispatch(addBook(data));
+        navigate("/")
+        
+    }
     return (
         <div className='p-5' >
-            <h2>this is add a book</h2>
+            <h2>Add a book</h2>
+            <form className='w-50 mt-5' onSubmit={handleSubmit(onSubmit)}>
+                
+                <div className="mb-3">
+                    <input className='form-control' placeholder='book title'  {...register("title", { required: true })} />
+                    {errors.review && <span className='text-danger' >Put a Title</span>}
+                    
+                </div>
+                <div className="mb-3">
+                    <input className='form-control' placeholder='author of the book' {...register("author", { required: true })} />
+                    {errors.review && <span className='text-danger' >Put a Author</span>}
+                </div>
+
+                <input className='btn btn-primary' value="Add" type="submit" />
+            </form>
         </div>
     );
 };
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,11 +1,63 @@
 import React from 'react';
-import { useSelector } from 'react-redux/es/exports';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteBook } from './BookSlice';
 
 const Books = () => {
     const books= useSelector((state)=> state.booksReducer.books);
+    const dispatch= useDispatch(); 
+
+    const handleDelete=(id)=>{
+         dispatch(deleteBook(id))
+    }
     return (
-        <div className='p-5' >
-            <h2>this is books {books.length} </h2>
+        <div className='container-fluid' >
+            <h2>All Books</h2>
+
+
+            <div className="row">
+
+                <div className="col-md-12">
+
+                    <div className="row p-5">
+                        <table className="table table-borderLess table-responsive">
+                            <thead>
+                                <tr>
+                                    <th className="text-secondary text-left" scope="col">Id</th>
+                                    <th className="text-secondary" scope="col">Title</th>
+                                    <th className="text-secondary" scope="col">Author</th>
+                                    <th className="text-secondary" scope="col">Action</th>
+                                    
+
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    books.map((book) =>
+
+                                        <tr key={book.id} >
+                                            <td>{book.id}</td>
+                                            
+                                            <td>{book?.title}</td>
+                                            <td>{book?.author}</td>
+                                            <td>
+                                                <button className="btn btn-primary me-1">Edit</button>
+                                                <button onClick={()=>{handleDelete(book.id)}} className="btn btn-danger">Delete</button>
+                                            </td>
+                                            
+                                            
+
+
+
+                                        </tr>
+                                    )
+
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
